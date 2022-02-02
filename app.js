a5eec496e057539ec48f8597ce7c12162dbf5823
Michael Fernandez Perez <michael_f2005@hotmail.com>
2022-02-01 21:52:44 -0500

    clear console a hello world message

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+console.clear();
 
 // MODULOS DE TERCEROS
 
 // MODULOS PROPIOS
 require('./config/colors');
+
+// MAIN 
+const main = () => {
+
+    console.log('Hello World!'.error);
+
+};
+
+main();
